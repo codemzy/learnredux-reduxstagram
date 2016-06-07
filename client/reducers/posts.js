97edbbfe4dbea3c6c84d8da76b4f090d5bcfb4af
Emma J <codemzy@gmail.com>
2016-06-07 13:40:18 +0000

    Finished vid 7 creating redux reducers

---
+++ client/reducers/posts.js
@@ -0,0 +1,12 @@
+// a reducer takes in two things:
+// 1. The action (info about what happened)
+// 2. Copy of current state
+
+// the first time it runs we set state to an empty array
+
+function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
+
+export default posts;
