b0f1defa9799c6e64240af1816f8704f221bc0c0
Emma J <codemzy@gmail.com>
2016-06-08 13:22:37 +0000

    Display comments with Comments component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const Comments = React.createClass({
+    
+    _renderComment(comment, index) {
+        return (
+          <div className="comment" key={index}>
+            <p>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment">&times;</button>
+            </p>
+          </div>
+        )
+    },
+    
     render() {
         return (
-            <div className="comment">
-                I'm the comments!!
+            <div className="comments">
+                {this.props.postComments.map(this._renderComment)}
             </div>
         );
     }
