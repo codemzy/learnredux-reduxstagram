00a07a337d5b95acf6a78a8bc649a9ddc200aca7
Emma J <codemzy@gmail.com>
2016-06-08 09:34:44 +0000

    Updated Photo and PhotoGrid components to display post descriptions on PhotoGrid and pass down props from PhotoGrid to Photo

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                {this.props.posts.map((post, index) => <Photo />)}
+                {this.props.posts.map((post, index) => <Photo {...this.props} key={index} index={index} post={post} />)}
             </div>
         );
     }
