3f0eaa1d783d65ff49de7ceaa3fa610a2f8ee19a
Emma J <codemzy@gmail.com>
2016-06-08 10:33:10 +0000

    Finished vid 12 updating state with reducers - build the first reducer for the increment likes action

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,9 +5,18 @@
 // the first time it runs we set state to an empty array
 
 function posts(state = [], action) {
-    console.log('the post will change');
-    console.log(state, action);
-    return state;
+    if (action.type === 'INCREMENT_LIKES') {
+        console.log('Incrementing likes');
+        // return the updated state
+        const index = action.index;
+        return [
+            ...state.slice(0, index), // before the one we are updating
+            {...state[index], likes: state[index].likes + 1}, // the one we are updating
+            ...state.slice(index + 1), // after the one we are updating
+        ];
+    } else {
+        return state;
+    }
 }
 
 export default posts;
