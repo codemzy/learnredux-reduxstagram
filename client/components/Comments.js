d21f7d27a8259d797715e26f414cd457f67d17e3
Emma J <codemzy@gmail.com>
2016-06-08 13:49:46 +0000

    Added a _handleSubmit function to our comment form and passed the postId via props from Single

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -11,14 +11,23 @@ const Comments = React.createClass({
                 <button className="remove-comment">&times;</button>
             </p>
           </div>
-        )
+        );
+    },
+    
+    _handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitting the form');
+        const postId = this.props.postId;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        console.log(postId, author, comment);
     },
     
     render() {
         return (
             <div className="comments">
                 {this.props.postComments.map(this._renderComment)}
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this._handleSubmit}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
