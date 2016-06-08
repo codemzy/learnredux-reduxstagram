5edd8c293526b48c3aaa571fb44d9c3420c4fdfa
Emma J <codemzy@gmail.com>
2016-06-08 12:37:56 +0000

    Updated Single component to get the post index from the route param

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,13 @@
 import React from 'react';
+// reuse the Photo component
+import Photo from './Photo';
 
 const Single = React.createClass({
     render() {
+        // index of the post
+        const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        console.log(index);
+        // get us the post
         return (
             <div className="single-photo">
                 I'm the single
